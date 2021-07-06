c5421bfe726e14fdbaad9082ffbf0834f7102e04
Lucas Dumer <lucasdumer@gmail.com>
2021-07-06 19:17:25 -0300

    adicionado domain Client

---
+++ app/Core/Domain/Client.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Core\Domain;
+
+class Client
+{
+    private int $id;
+
+    public function __construct(
+        private string $name,
+        private string $gender,
+        private \DateTime $birth,
+        private Address $address
+    ) {}
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getGender(): string
+    {
+        return $this->gender;
+    }
+
+    public function setGender(string $gender): void
+    {
+        $this->gender = $gender;
+    }
+
+    public function getBirth(): \DateTime
+    {
+        return $this->birth;
+    }
+
+    public function setBirth(\DateTime $birth): void
+    {
+        $this->birth = $birth;
+    }
+
+    public function getAddress(): Address
+    {
+        return $this->address;
+    }
+
+    public function setAddress(Address $address): void
+    {
+        $this->address = $address;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            "name" => $this->name,
+            "gender" => $this->gender,
+            "birth" => $this->birth->format("Y-m-d"),
+            "address" => $this->address->toArray()
+        ];
+    }
+}
