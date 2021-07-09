c096478885fc919c2e1ca2a6fd2fdcc90ba6961b
Lucas Dumer <lucasdumer@gmail.com>
2021-07-09 13:15:38 -0300

    adicionado interface de repository

--- app/Core/UseCases/CreateClient.php
+++ app/Core/UseCases/CreateClient.php
@@ -4,9 +4,14 @@ namespace App\Core\UseCases;
 
 use App\Core\Domain\Address;
 use App\Core\Domain\Client;
+use App\Core\Repositories\IClientRepository;
 
 class CreateClient
 {
+    public function __construct(
+        private IClientRepository $clientRepository
+    ) {}
+
     public function execute(
         string $name,
         string $gender,
@@ -19,7 +24,21 @@ class CreateClient
         string $number,
         string $complement
     ): Client {
-        $client = new Client($name, $gender, $birth, new Address($country, $state, $city, $district, $street, $number, $complement));
+        $client = new Client(
+            $name,
+            $gender,
+            $birth,
+            new Address(
+                $country,
+                $state,
+                $city,
+                $district,
+                $street,
+                $number,
+                $complement
+            )
+        );
+        $client = $this->clientRepository->create($client);
         return $client;
     }
 }
